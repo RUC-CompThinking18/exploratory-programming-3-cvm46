5888490e873175c0e16d4bdcf65b73a36e8f9748
cvm46 <cvm46@scarletmail.rutgers.edu>
2018-10-25 16:15:15 -0400

    Update Exploritory3-CharlesMarinelli.py
    yO DAWG I HEARD YOU LIKE COMMENTS SO I COMMENTED ON YOUR COMMENTS AND YER COMMENTS WITH COMMENTS AND COMMENTS!

--- Exploritory3-CharlesMarinelli.py
+++ Exploritory3-CharlesMarinelli.py
@@ -3,11 +3,11 @@ infile = open('faust.txt', 'r')
 text= infile.read()
 def satanitcRegex(word):
     if type(word) != str:
-        raise TypeError("EYY DUMBASS THIS AIN'T A STRING!")
+        raise TypeError("EYY DUMBASS THIS AIN'T A STRING!")#this new york style error is necessary to show the user what they did wrong.
     matches =re.findall(r"\w*at\b",text)
     Jeffery=[]
     for word in matches:
         if len(word) > 3:
             Jeffery.append(word)
-    print Jeffery
-satanicRegex(text)
+    print Jeffery#rather than returning the list just printing it will show us the results.
+satanicRegex(text)#THIS IS NECESSARY FOR THE GODDAMN THING TO RUN! DO NOT FORGET IT! DO NOT!
